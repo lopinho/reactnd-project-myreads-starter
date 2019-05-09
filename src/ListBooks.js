5b14bda6f6719928749074f82b1b50c73d637fa6
André Ferreira <lopinho@gmail.com>
2019-05-09 19:34:32 -0300

    Adicionando react-router-dom ao projeto, e alterando componentes e app para utiliza-lo

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class ListBooks extends React.Component {
     render () {
@@ -159,7 +160,7 @@ class ListBooks extends React.Component {
                     </div>
                 </div>
                 <div className="open-search">
-                    <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+                    <Link to="/search"><button>Add a book</button></Link>
                 </div>
             </div>
         )
